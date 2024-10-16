05351cab866ebbce2a39c01e7a58e48ed6b77d08
Pleblel <pelleaugust07@outlook.com>
2024-10-16 09:45:49 +0200

    Bunker aplha death godlike destruction

--- Assets/Scripts/Bunker.cs
+++ Assets/Scripts/Bunker.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Timeline;
 using UnityEngine;
+using UnityEngine.U2D;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class Bunker : MonoBehaviour
 {
     public int nrOfHits = 0;
     public List<Transform> Child = new List<Transform>();
+    public SpriteShapeRenderer sSRender;
+    float fadeDuration = 2.0f;
     private void Awake()
     {
         Child = new List<Transform>(GetComponentsInChildren<Transform>());
         Child.RemoveAt(0);
         int lastIndex = Child.Count - 1;
         Child.RemoveAt(lastIndex);
+
+        sSRender = GetComponentInChildren<SpriteShapeRenderer>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -33,8 +38,10 @@ public class Bunker : MonoBehaviour
                     childTransform.gameObject.GetComponent<DistanceJoint2D>().enabled = false;
 
                     Rigidbody2D pointRB = childTransform.gameObject.GetComponent<Rigidbody2D>();
-                    pointRB.velocity = new Vector2(transform.position.x - childTransform.position.x, transform.position.y - childTransform.position.y);
+                    pointRB.velocity = new Vector2((transform.position.x - childTransform.position.x) * 3, (transform.position.y - childTransform.position.y) * 3);
+                    
                 }
+                DestroyBunker();
             }
             
         }
@@ -44,5 +51,20 @@ public class Bunker : MonoBehaviour
     {
         gameObject.SetActive(true);
         nrOfHits = 0; 
+    }
+
+    IEnumerator DestroyBunker ()
+    {
+        Color originalColor = sSRender.color;
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(originalColor.a, 0, elapsedTime / fadeDuration);
+            sSRender.color = new Color(originalColor.r, originalColor.g, originalColor.b, newAlpha);
+            yield return null;
+        }
+
+        sSRender.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0);
     }
 }
