95a72bd02dcbe8bd7d1bdae05f61a5f5ae8246fc
Darren Scott <darrenpipi8@gmail.com>
2024-10-23 09:52:41 +0200

    Boss fight flip

--- Assets/BossLaser.cs
+++ Assets/BossLaser.cs
@@ -5,19 +5,42 @@ using UnityEngine;
 public class BossLaser : Projectile
 {
 
+     public Rigidbody2D rb; 
+
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = transform.forward * speed; 
+        rb = GetComponent<Rigidbody2D>();
+        Vector2 Mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rb = GetComponent<Rigidbody2D>();
+        Vector2 direction = new Vector2(Mouseposition.x - transform.position.x, Mouseposition.y - transform.position.y).normalized;
+        rb.velocity = direction.normalized * speed;
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        CheckCollision(collision);
+    }
 
+    void CheckCollision(Collider2D collision)
+    {
+        /*/Bunker bunker = collision.gameObject.GetComponent<Bunker>();
+
+        if(bunker == null) //Om det inte är en bunker vi träffat så ska skottet försvinna.
+        {
+            Destroy(gameObject);
+        }/*/
+        Destroy(gameObject);
+    }
 
 }
